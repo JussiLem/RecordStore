652918212dcfb70191ad34439905b2e50bc4d69c
Jussi Lemmetyinen <jussi.lem@gmail.com>
2018-11-05 18:57:44 +0200

    create schema for artist

---
+++ src/main/java/recordstore/schema/ArtistSchemaSql.java
@@ -0,0 +1,11 @@
+package recordstore.schema;
+
+public class ArtistSchemaSql {
+
+  private ArtistSchemaSql() {}
+
+  public static final String CREATE_SCHEMA_SQL =
+      "CREATE TABLE ARTISTS (ID NUMBER, NAME VARCHAR(100))";
+
+  public static final String DELETE_SCHEMA_SQL = "DROP TABLE ARTISTS";
+}
