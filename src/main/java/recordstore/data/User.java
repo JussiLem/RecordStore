db544c9e9d6662f45c0129d483a9ae4c48933de9
Jussi Lemmetyinen <jussi.lem@gmail.com>
2018-11-10 18:05:34 +0200

    User data

--- src/main/java/recordstore/data/User.java
+++ src/main/java/recordstore/data/User.java
@@ -4,15 +4,15 @@ public class User implements java.io.Serializable {
 
     private String name;
 
+    public User(String name) {
+        this.name = name;
+    }
+
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
-        this.name = name;
-    }
-
-    public User(String name) {
         this.name = name;
     }
 }
