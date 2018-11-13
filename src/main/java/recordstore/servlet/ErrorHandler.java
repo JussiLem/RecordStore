664e13ec9069d6697ccae6eaf89c696f8ad8a8e8
Jussi Lemmetyinen <jussi.lem@gmail.com>
2018-11-13 17:10:08 +0200

    Error handler

--- src/main/java/recordstore/servlet/ErrorHandler.java
+++ src/main/java/recordstore/servlet/ErrorHandler.java
@@ -1,4 +1,39 @@
 package recordstore.servlet;
 
-public class ErrorHandler {
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import static javax.servlet.RequestDispatcher.*;
+
+@WebServlet(name = "ErrorHandler", urlPatterns = "/ErrorHandler")
+public class ErrorHandler extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws IOException {
+        resp.setContentType("text/html; charset=utf-8");
+        try (PrintWriter writer = resp.getWriter()) {
+            writer.write("<html><head><title>Error description</title></head><body>");
+            writer.write("<h2>Error description</h2>");
+            writer.write("<ul>");
+            Arrays.asList(ERROR_STATUS_CODE, ERROR_EXCEPTION_TYPE, ERROR_MESSAGE)
+                    .forEach(e ->
+                            writer.write("<li>" + e + ":" + req.getAttribute(e) + " </li>")
+                    );
+            writer.write("</ul>");
+            writer.write("</html></body>");
+        }
+
+        Exception exception = (Exception) req.getAttribute(ERROR_EXCEPTION);
+        if (exception instanceof IllegalArgumentException) {
+            getServletContext().log("Error on an application argument", exception);
+        }
+    }
+
 }
