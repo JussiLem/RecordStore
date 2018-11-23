86f91d7fa64020133f40bb44ef4e3fc471bb9b7f
Jussi Lemmetyinen <jussi.lem@gmail.com>
2018-11-23 21:13:14 +0200

    fixed the main class

--- src/main/java/recordstore/Application.java
+++ src/main/java/recordstore/Application.java
@@ -1,11 +1,9 @@
 package recordstore;
 
-
 import org.h2.jdbcx.JdbcDataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import recordstore.dao.ArtistDao;
-import recordstore.dao.DbArtistDao;
 import recordstore.dao.InMemoryArtistDao;
 import recordstore.data.Artist;
 import recordstore.schema.ArtistSchemaSql;
@@ -23,35 +21,34 @@ public class Application {
     private static final String URL = "jdbc:h2:~/dao";
 
     /**
-     * Program entry point.
+     * Program entry point for testing.
      *
      * @param args command line args.
-     * @throws Exception jos virheitä.
      */
-    public static void main(final String[] args) throws Exception {
+    public static void main(final String[] args) throws SQLException {
 
         final ArtistDao inMemoryDao = new InMemoryArtistDao();
         performOperationsUsing(inMemoryDao);
 
         final DataSource dataSource = createDataSource();
         createSchema(dataSource);
-        final ArtistDao dbDao = new DbArtistDao(dataSource);
-        performOperationsUsing(dbDao);
-        deleteSchema(dataSource);
+       // final ArtistDao dbDao = new DbArtistDao(dataSource);
+      //  performOperationsUsing(dbDao);
+        //deleteSchema(dataSource);
 
   }
 
     private static void deleteSchema(DataSource dataSource) throws SQLException {
         try (Connection connection = dataSource.getConnection();
              Statement statement = connection.createStatement()) {
-            statement.execute(ArtistSchemaSql.DELETE_SCHEMA_SQL);
+            statement.execute(ArtistSchemaSql.DROP_TABLE_ARTISTS);
         }
     }
 
     private static void createSchema(DataSource dataSource) throws SQLException {
         try (Connection connection = dataSource.getConnection();
              Statement statement = connection.createStatement()) {
-            statement.execute(ArtistSchemaSql.CREATE_SCHEMA_SQL);
+            statement.execute(ArtistSchemaSql.CREATE_TABLE_ARTISTS);
         }
     }
 
@@ -68,23 +65,23 @@ public class Application {
       try (Stream<Artist> artistStream = artistDao.getAll()) {
           artistStream.forEach(artist -> LOGGER.info(String.valueOf(artist)));
       }
-      LOGGER.info("artistDao.getArtistById(2): {}" + artistDao.getById(2));
+      LOGGER.info("artistDao.getArtistById(2): {}", artistDao.getById(2));
       final Artist artist = new Artist(4, "Kid Rock");
       artistDao.add(artist);
-      LOGGER.info("artistDao.getAllArtists(): {}" + artistDao.getAll());
+      LOGGER.info("artistDao.getAllArtists(): {}", artistDao.getAll());
       artist.setName("Robert James Ritchie");
       artistDao.update(artist);
       LOGGER.info("artistDao.getAllArtists(): ");
       try (Stream<Artist> artistStream = artistDao.getAll()) {
-          artistStream.forEach((art) -> LOGGER.info(String.valueOf(art)));
+          artistStream.forEach(art -> LOGGER.info(String.valueOf(art)));
       }
       artistDao.delete(artist);
-      LOGGER.info("artistDao.getAllArtists: {}" + artistDao.getAll());
+      LOGGER.info("artistDao.getAllArtists: {}", artistDao.getAll());
   }
 
   private static void addArtists(ArtistDao artistDao) throws SQLException {
-      for (Artist customer : generateSampleArtists()) {
-          artistDao.add(customer);
+      for (Artist artist : generateSampleArtists()) {
+          artistDao.add(artist);
       }
   }
 
