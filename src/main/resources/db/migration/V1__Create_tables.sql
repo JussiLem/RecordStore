67a169258b3337a9d89d5508c92558895130d5fb
Jussi Lemmetyinen <jussi.lem@gmail.com>
2018-12-10 17:38:49 +0200

    sql table fixes

--- src/main/resources/db/migration/V1__Create_tables.sql
+++ src/main/resources/db/migration/V1__Create_tables.sql
@@ -1,33 +1,40 @@
 -- Artists
+DROP TABLE IF EXISTS `artists`;
 CREATE TABLE `artists`
 (
-  `id`   INT AUTO_INCREMENT NOT NULL UNIQUE,
-  `name` VARCHAR(100) NOT NULL UNIQUE,
-  PRIMARY KEY (`name`)
+  `id`   INT AUTO_INCREMENT NOT NULL,
+  `name` VARCHAR(100)       NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = UTF8mb4;
 
 -- Albums
+DROP TABLE IF EXISTS `albums`;
 CREATE TABLE `albums`
 (
-  `albumId` INT AUTO_INCREMENT NOT NULL UNIQUE COMMENT 'albumin tunnus id',
-  `name`    VARCHAR(100) DEFAULT NULL COMMENT 'albumin nimi',
-  `artist`  VARCHAR(100) DEFAULT NULL COMMENT 'artisti, jolle albumi kuuluu',
+  `albumId`  INT AUTO_INCREMENT NOT NULL COMMENT 'albumin tunnus id',
+  `name`     VARCHAR(100)       NOT NULL COMMENT 'albumin nimi',
+  `artistId` INT,
+  `artist`   VARCHAR(100) COMMENT 'artisti, jolle albumi kuuluu',
   PRIMARY KEY (`albumId`),
   KEY (`name`),
-  FOREIGN KEY (`artist`) REFERENCES artists (`name`)
+  KEY (`artistId`),
+  CONSTRAINT `albums_ibfk_1` FOREIGN KEY (`artistId`) REFERENCES `artists` (`id`) ON DELETE SET NULL
 ) ENGINE = InnoDB
   DEFAULT CHARSET = UTF8mb4;
 
 -- Tracks
+DROP TABLE IF EXISTS `tracks`;
 CREATE TABLE `tracks`
 (
-  `trackId` INT          NOT NULL UNIQUE,
-  `track`   VARCHAR(100) NOT NULL COMMENT 'biisin nimi',
-  `artist`  VARCHAR(100) NOT NULL COMMENT 'artisti, jolle biisi kuuluu',
-  `album`   VARCHAR(100) COMMENT 'albumi(jos on), jolle biisi kuuluu',
-  PRIMARY KEY (`track`),
-  FOREIGN KEY (`artist`) REFERENCES artists (`name`),
-  FOREIGN KEY (`album`) REFERENCES albums (`name`)
+  `trackId`  INT AUTO_INCREMENT NOT NULL UNIQUE,
+  `track`    VARCHAR(100)       NOT NULL COMMENT 'biisin nimi',
+  `artistId` INT,
+  `artist`   VARCHAR(100) COMMENT 'artisti, jolle biisi kuuluu',
+  `albumId`  INT COMMENT 'albumin tunnus id',
+  `album`    VARCHAR(100) COMMENT 'albumi(jos on), jolle biisi kuuluu',
+  PRIMARY KEY (`trackId`),
+  CONSTRAINT `tracks_ibfk_1` FOREIGN KEY (`artistId`) REFERENCES `artists` (`id`) ON DELETE SET NULL,
+  CONSTRAINT `tracks_ibfk_2` FOREIGN KEY (`albumId`) REFERENCES `albums` (`albumId`) ON DELETE SET NULL
 ) ENGINE = InnoDB
   DEFAULT CHARSET = UTF8mb4;
